18728ed81f77ed44c6f357c33ea17ea92b424d68
dawitt2327 <dawitt2327@gmail.com>
2023-11-22 00:25:34 +0300

    sign up feature added

--- src/pages/landingPage/Header/Header.jsx
+++ src/pages/landingPage/Header/Header.jsx
@@ -24,7 +24,7 @@ const Header = ({ selectedNav }) => {
             type="button"
             className="md:hidden text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  mx-3 md:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
           >
-            Sign Up
+            Sign Uppppp
           </button>
           <button
             data-collapse-toggle="navbar-sticky"
